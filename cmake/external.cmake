4cfa657fe33c062aa75ae2bd7a4657c36c63490a
frostzt <aidenfrostbite@gmail.com>
2026-03-21 19:38:44 +0530

    created cmake modules and txt files

---
+++ cmake/external.cmake
@@ -0,0 +1,12 @@
+# --- External Content ---
+
+include(FetchContent)
+
+FetchContent_Declare(
+  googletest
+  GIT_REPOSITORY https://github.com/google/googletest.git
+  GIT_TAG        v1.17.0
+)
+
+# Populate the content
+FetchContent_MakeAvailable(googletest)
