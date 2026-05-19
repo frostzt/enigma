910f4bb11d97bd230bf20ae8b400d42934ac2978
frostzt <aidenfrostbite@gmail.com>
2026-05-19 15:38:32 +0530

    added test for flush and read sstable

--- tests/storage/storage_engine_test.cc
+++ tests/storage/storage_engine_test.cc
@@ -35,3 +35,37 @@ TEST(StorageEngine, basic_flow) {
     auto value_str = bytes_to_string(value.data);
     ASSERT_EQ(value_str, "1234");
 }
+
+TEST(StorageEngine, flush_and_read_sstable) {
+    PosixIOEngine engine;
+    std::string data_dir_path = "./storage_engine_tests";
+    Tempdir testdir(data_dir_path);
+
+    auto storage_engine_result =
+        StorageEngine::open(engine, data_dir_path, 512);
+    ASSERT_TRUE(storage_engine_result.has_value());
+
+    auto& storage_engine = storage_engine_result.value();
+
+    for (size_t i = 10; i < 60; ++i) {
+        ASSERT_TRUE(storage_engine
+                        .put(string_to_bytes("alice"),
+                             string_to_bytes("2026-" + std::to_string(i)),
+                             "age" + std::to_string(i),
+                             string_to_bytes("12" + std::to_string(i)))
+                        .has_value());
+    }
+
+    for (size_t i = 10; i < 60; ++i) {
+        auto result =
+            storage_engine.get(string_to_bytes("alice"),
+                               string_to_bytes("2026-" + std::to_string(i)),
+                               "age" + std::to_string(i));
+
+        ASSERT_TRUE(result.has_value());
+        ASSERT_TRUE(result.value().has_value());
+
+        auto value = result.value().value();
+        ASSERT_EQ(bytes_to_string(value.data), "12" + std::to_string(i));
+    }
+}
