667018ccc82f73b9ac2c0353b0635615906f8bd3
frostzt <aidenfrostbite@gmail.com>
2026-06-01 00:13:42 +0530

    bump filter to 2

--- src/storage/sstable/sstable_reader.cpp
+++ src/storage/sstable/sstable_reader.cpp
@@ -64,7 +64,7 @@ SSTExpectResult<SSTableReader> SSTableReader::create(io::IOEngine& engine,
     // filter block
     auto filter_block_size = common::decode_uint32(footer_buffer.data(), 20);
 
-    if (filter_block_size < 1) {
+    if (filter_block_size < 2) {
         return SSTExpectResult<SSTableReader>::err(common::Error{
             common::ErrorCode::BAD_CONFIG, "invalid filter block size"});
     }
